a7d279f15f1b4e9c24b04419d17f1ae900467502
fernando <fernando.h.vinha@gmail.com>
2021-02-11 16:20:42 -0300

    teste

--- api.py
+++ api.py
@@ -1,7 +1,10 @@
 from flask import Flask
+
+
 app = Flask("api")
 @app.route("/api", methods=["GET"])
 def rreturn():
     return{"funcionado"}
- 
-    app.run(host='0.0.0.0', port=80)
+
+
+app.run()
